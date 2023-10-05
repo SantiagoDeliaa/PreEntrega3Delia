c978dd8a5f077029fff021986f4f29ebd20d1cb6
Santiago Delía <115126313+SantiagoDeliaa@users.noreply.github.com>
2023-10-05 19:49:31 -0300

    Update index.js Pre entrega 3

--- index.js
+++ index.js
@@ -76,3 +76,84 @@ function actualizarTotalGastos() {
     const total = gastos.reduce((acumulador, gasto) => acumulador + gasto.cantidad, 0);
     totalGastos.textContent = total.toFixed(2);
 }
+
+// Definir eventos y sus funciones de respuesta
+listaTareas.addEventListener("click", (event) => {
+    if (event.target.tagName === "LI") {
+        event.target.classList.toggle("completada");
+        actualizarTareasLocalStorage();
+    }
+});
+
+listaTareas.addEventListener("dblclick", (event) => {
+    if (event.target.tagName === "LI") {
+        const tareaTexto = event.target.textContent;
+        const nuevoTexto = prompt("Editar tarea:", tareaTexto);
+        if (nuevoTexto !== null && nuevoTexto.trim() !== "") {
+            event.target.textContent = nuevoTexto;
+            actualizarTareasLocalStorage();
+        }
+    }
+});
+
+listaTareas.addEventListener("click", (event) => {
+    if (event.target.tagName === "BUTTON") {
+        event.target.parentElement.remove();
+        actualizarTareasLocalStorage();
+    }
+});
+
+listaGastos.addEventListener("click", (event) => {
+    if (event.target.tagName === "BUTTON") {
+        event.target.parentElement.remove();
+        actualizarGastosLocalStorage();
+    }
+});
+
+// Modificar el DOM para reflejar las acciones de los usuarios
+function actualizarTareasLocalStorage() {
+    const tareas = Array.from(listaTareas.children).map((tarea) => {
+        return {
+            texto: tarea.textContent,
+            completada: tarea.classList.contains("completada"),
+        };
+    });
+    localStorage.setItem("tareas", JSON.stringify(tareas));
+}
+
+function actualizarGastosLocalStorage() {
+    const gastos = Array.from(listaGastos.children).map((gasto) => {
+        const partes = gasto.textContent.split(": $");
+        return {
+            categoria: partes[0],
+            cantidad: parseFloat(partes[1]),
+        };
+    });
+    localStorage.setItem("gastos", JSON.stringify(gastos));
+}
+
+// Almacenar datos en localStorage y recuperarlos
+document.addEventListener("DOMContentLoaded", () => {
+    const tareas = JSON.parse(localStorage.getItem("tareas")) || [];
+    tareas.forEach((tarea) => {
+        const li = document.createElement("li");
+        li.textContent = tarea.texto;
+        if (tarea.completada) {
+            li.classList.add("completada");
+        }
+        const botonEliminar = document.createElement("button");
+        botonEliminar.textContent = "Eliminar";
+        li.appendChild(botonEliminar);
+        listaTareas.appendChild(li);
+    });
+
+    const gastos = JSON.parse(localStorage.getItem("gastos")) || [];
+    gastos.forEach((gasto) => {
+        const li = document.createElement("li");
+        li.textContent = `${gasto.categoria}: $${gasto.cantidad.toFixed(2)}`;
+        const botonEliminar = document.createElement("button");
+        botonEliminar.textContent = "Eliminar";
+        li.appendChild(botonEliminar);
+        listaGastos.appendChild(li);
+    });
+});
